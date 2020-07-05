cc810f31aee4942ba628671994bc409f66866f97
AlvaradoAlexis <eaalvara@espol.edu.ec>
2020-07-05 01:02:22 -0500

    prueba de javaFX

--- Sistema-de-Turnos/src/com/company/Main/SistemaDeTurnos.java
+++ Sistema-de-Turnos/src/com/company/Main/SistemaDeTurnos.java
@@ -5,17 +5,33 @@
  */
 package com.company.Main;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Alexis
  */
-public class SistemaDeTurnos {
-
+public class SistemaDeTurnos extends Application{
+    
+    @Override
+    public void start(Stage arg0) throws Exception {
+        Pane root=new Pane();
+        root.getChildren().add(new Label("hola mundo"));
+        Scene sc = new Scene(root,730,550);
+        arg0.setTitle("prueba");
+        arg0.setScene(sc);
+        arg0.show();
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Proyecto de estructura de datos");
+        launch(args);
     }
+
     
 }
