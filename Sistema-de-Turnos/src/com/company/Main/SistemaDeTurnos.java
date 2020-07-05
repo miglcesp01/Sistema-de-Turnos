d71d6da8ef3ae8205440700e256c92915c682a89
Miguel <angelmigue.cespedes@gmail.com>
2020-07-05 00:30:04 -0500

    Probando proyecto con IntelliJ

--- Sistema-de-Turnos/src/com/company/Main/SistemaDeTurnos.java
+++ Sistema-de-Turnos/src/com/company/Main/SistemaDeTurnos.java
@@ -15,7 +15,8 @@ public class SistemaDeTurnos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Proyecto de estructura de datos");
+
+        System.out.println("Proyecto de estructura de datos. 100 asegurado");
     }
     
 }
