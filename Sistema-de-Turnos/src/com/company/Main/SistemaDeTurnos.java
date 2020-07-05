6075108f4a29b372f503dc05b33f876518ee46a6
AlvaradoAlexis <eaalvara@espol.edu.ec>
2020-07-05 00:18:41 -0500

    cambio de ide

--- Sistema-de-Turnos/src/com/company/Main/SistemaDeTurnos.java
+++ Sistema-de-Turnos/src/com/company/Main/SistemaDeTurnos.java
@@ -15,7 +15,7 @@ public class SistemaDeTurnos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Proyecto de estructura de datos");
     }
     
 }
